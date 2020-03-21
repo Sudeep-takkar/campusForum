6abadb8b7ce3a88991925f721088b6ddd26fbc6b
sudeep-takkar <sudeep.takkar@yahoo.com>
2020-03-21 13:47:52 -0400

    initial commits

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
   GITHUB_CLIENT_ID: 'e519e77da50d51a44218',
   GITHUB_CLIENT_SECRET: 'c7956e4504b00749a1af531092dc34cf863065fd',
-  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  GITHUB_CALLBACK_URL: 'https://campus-forum.herokuapp.com/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://dbUser:Sudeep%401993@cluster0-shard-00-00-vtzyo.gcp.mongodb.net:27017,cluster0-shard-00-01-vtzyo.gcp.mongodb.net:27017,cluster0-shard-00-02-vtzyo.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
 };
